8e60883efa1067c6d3ffd78b832f806cc359e4c1
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-17 11:09:57 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ahsan Qureshi on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
